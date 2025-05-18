22aca1b31ad1ddf43b65ec46c023e1004a395c7f
Molnár Balázs <molbalu22@gmail.com>
2025-05-18 17:12:28 +0200

    Websocket starter

--- game-ui/src/main.ts
+++ game-ui/src/main.ts
@@ -1,8 +1,5 @@
 import "./style.css";
-import { setupCounter } from "./counter.ts";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <h1 class="text-2xl text-neutral-900 dark:text-neutral-100">Game UI</h1>
 `;
-
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,3 +1,18 @@
 import config from "./config.js";
 
+import { WebSocketServer } from "ws";
+
 console.log(config);
+
+const wss = new WebSocketServer({ host: "0.0.0.0", port: 8080 });
+
+wss.on("connection", function connection(ws) {
+  ws.on("error", console.error);
+
+  ws.on("message", function message(data) {
+    console.log("received: %s", data);
+    ws.send("roger");
+  });
+
+  ws.send("something");
+});
