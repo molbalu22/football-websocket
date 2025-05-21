c184c6d696ad28c53dca0d83dd499d9926efb642
Molnár Balázs <molbalu22@gmail.com>
2025-05-21 19:49:03 +0200

    Websocket - Player join functionality

--- game-ui/src/main.ts
+++ game-ui/src/main.ts
@@ -1,5 +1,73 @@
 import "./style.css";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <h1 class="text-2xl text-neutral-900 dark:text-neutral-100">Game UI</h1>
-`;
+const DEBUG = true;
+const WEBSOCKET_URL = "http://localhost:8080";
+
+const gameStatusElement = document.getElementById("gameStatus");
+
+type PlayerAcceptedMessage = {
+  type: "playerAccepted";
+  playerId: string;
+  playerIndex: number;
+};
+
+type TooManyPlayersMessage = {
+  type: "tooManyPlayers";
+};
+
+type GameMessage = PlayerAcceptedMessage | TooManyPlayersMessage;
+
+const gameState = {
+  playerIndex: 0,
+};
+
+function handleMessage(ws: WebSocket, message: GameMessage) {
+  // Will be useful later
+  ws;
+
+  if (message.type === "playerAccepted") {
+    sessionStorage.setItem("playerId", message.playerId);
+    gameState.playerIndex = message.playerIndex;
+
+    if (gameStatusElement) {
+      gameStatusElement.textContent = `You are: Player ${gameState.playerIndex}`;
+    }
+  } else if (message.type === "tooManyPlayers") {
+    if (gameStatusElement) {
+      gameStatusElement.textContent =
+        "Sorry, you can't join now. There are too many players in the game.";
+    }
+  }
+}
+
+async function connectToWebsocket() {
+  const ws = new WebSocket(WEBSOCKET_URL);
+
+  ws.onmessage = (event) => {
+    const message = JSON.parse(event.data);
+
+    if (DEBUG) {
+      console.log("[DEBUG] Websocket message received:");
+      console.log(message);
+    }
+
+    handleMessage(ws, message);
+  };
+
+  ws.onopen = () => {
+    console.log("Websocket connection established");
+
+    ws.send(
+      JSON.stringify({
+        type: "playerReady",
+        playerId: sessionStorage.getItem("playerId"),
+      })
+    );
+  };
+}
+
+try {
+  connectToWebsocket();
+} catch {
+  console.error("Can't connect to the websocket");
+}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,75 @@
 import config from "./config.js";
 
-import { WebSocketServer } from "ws";
+import { randomBytes } from "node:crypto";
+import { WebSocket, WebSocketServer } from "ws";
+
+function generateToken(length = 56) {
+  return Buffer.from(randomBytes(length)).toString("hex");
+}
 
 console.log(config);
+
+type Player = { socket: WebSocket; active: boolean; playerId: string };
+
+let players: Array<Player> = [];
+
+type PlayerReadyMessage = {
+  type: "playerReady";
+  playerId: string | null;
+};
+
+type GameMessage = PlayerReadyMessage;
+
+function handleMessage(ws: WebSocket, message: GameMessage) {
+  if (message.type === "playerReady") {
+    const playerIndex = players.findIndex(
+      (player) => player.playerId === message.playerId
+    );
+
+    if (playerIndex !== -1) {
+      const player = players[playerIndex];
+
+      player.socket = ws;
+      player.active = true;
+
+      ws.send(
+        JSON.stringify({
+          type: "playerAccepted",
+          playerId: message.playerId,
+          playerIndex,
+        })
+      );
+
+      return;
+    }
+
+    if (players.length === 2) {
+      ws.send(
+        JSON.stringify({
+          type: "tooManyPlayers",
+        })
+      );
+
+      return;
+    }
+
+    const playerId = generateToken();
+
+    players.push({
+      socket: ws,
+      active: true,
+      playerId: playerId,
+    });
+
+    ws.send(
+      JSON.stringify({
+        type: "playerAccepted",
+        playerId,
+        playerIndex: players.length - 1,
+      })
+    );
+  }
+}
 
 const wss = new WebSocketServer({ host: "0.0.0.0", port: 8080 });
 
@@ -11,8 +78,16 @@ wss.on("connection", function connection(ws) {
 
   ws.on("message", function message(data) {
     console.log("received: %s", data);
-    ws.send("roger");
+    handleMessage(ws, JSON.parse(data.toString()));
   });
 
-  ws.send("something");
+  ws.onclose = () => {
+    for (const player of players) {
+      if (player.socket === ws) {
+        player.active = false;
+      }
+    }
+  };
+
+  ws.send(JSON.stringify({ type: "serverHello" }));
 });
